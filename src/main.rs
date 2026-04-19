e6ba8d4f12e238ff6b3e5ee969036062cd909603
Jack Smith <jackwsmth@proton.me>
2026-04-19 11:23:10 +0400

    Update tokenize logic and create build_token function

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,42 @@ fn main() {
     println!("{:?}", tokens);
 }
 
-fn tokenize(expr: &str) -> Vec<&str> {
-    let words: Vec<&str> = expr.split_whitespace().collect();
-    words
+fn tokenize(expr: &str) -> Vec<Token> {
+    let mut tokens: Vec<Token> = vec![];
+    let mut current_number = String::new();
+
+    for c in expr.chars() {
+        if c.is_ascii_digit() {
+            current_number.push(c);
+        } else {
+            if !current_number.is_empty() {
+                tokens.push(build_token(TokenType::INT, current_number.clone()));
+                current_number.clear();
+            }
+        }
+
+        match c {
+            ' ' => continue,
+            '(' => tokens.push(build_token(TokenType::LPAREN, String::from("("))),
+            ')' => tokens.push(build_token(TokenType::RPAREN, String::from(")"))),
+            '+' => tokens.push(build_token(TokenType::ADD, String::from("+"))),
+            '-' => tokens.push(build_token(TokenType::SUB, String::from("-"))),
+            '*' => tokens.push(build_token(TokenType::MUL, String::from("*"))),
+            '/' => tokens.push(build_token(TokenType::DIV, String::from("/"))),
+            _ => continue,
+        }
+    }
+
+    if !current_number.is_empty() {
+        tokens.push(build_token(TokenType::INT, current_number));
+    }
+
+    tokens
+}
+
+fn build_token(token_type: TokenType, token_value: String) -> Token {
+    Token {
+        token_type,
+        token_value,
+    }
 }
